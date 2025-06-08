1414f222f19ed873c537eb0e9de0e05aa05e310c
igoroliveira21 <igoron182017@gmail.com>
2025-06-08 10:53:46 -0300

    Consultas com JPQL usando JOIN FETCH

--- src/main/java/com/example/demo/repositories/ProductRepository.java
+++ src/main/java/com/example/demo/repositories/ProductRepository.java
@@ -1,9 +1,15 @@
 package com.example.demo.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.entities.Product;
+import org.springframework.data.jpa.repository.Query;
+
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+    @Query("SELECT obj FROM Product obj JOIN FETCH obj.categories")
+    Page<Product> findProductsCategories(Pageable pageable);
 }
